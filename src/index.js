9e9e980e816634f7a509236303cd57aea482270a
Pmichelangelo <Pmichelangelo1377@gmail.com>
2024-02-25 21:42:36 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook%22">
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <App />
       </BrowserRouter>
     </Provider>
